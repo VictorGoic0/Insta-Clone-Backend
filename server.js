1382c966e6a7d38579f2159e70f2cde2518ecf8c
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-30 20:26:25 -0400

    Router for posts completed, used in server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
+const postRouter = require("./routers/postRouter.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use("/api/posts", postRouter);
 
 module.exports = server;
