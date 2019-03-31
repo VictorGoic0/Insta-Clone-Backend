deca8df6a78c4ef40f9f7971112e3fecf416daf2
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-30 20:03:43 -0400

    Base structure of tables created

--- data/migrations/20190330194118_Posts.js
+++ data/migrations/20190330194118_Posts.js
@@ -1,8 +1,55 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable("profiles", table => {
+      table.increments();
+      table
+        .string("username", 128)
+        .notNullable()
+        .unique();
+      table.string("password", 20).notNullable();
+      table.string("thumbnailUrl", 256);
+    })
+    .createTable("posts", table => {
+      table.increments();
+      table
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("profiles")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.string("imageUrl", 256);
+      table.integer("likes");
+    })
+    .createTable("comments", table => {
+      table.increments();
+      table
+        .string("text", 500)
+        .notNullable()
+        .unique();
+      table
+        .integer("post_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("posts")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("profiles")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists("comments")
+    .dropTableIfExists("posts")
+    .dropTableIfExists("profiles");
 };
